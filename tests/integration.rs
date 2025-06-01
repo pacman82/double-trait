46a2608639f7023cfc988b325ddc3bc1d345e36f
Markus Klein <markus-klein@live.de>
2025-06-01 16:33:08 +0200

    test: Allow dead code in integration tests

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,9 +1,10 @@
+#![allow(dead_code)]
+
 use derive_double::double;
 
 #[double(MyEmptyTraitDummy)]
 trait MyEmptyTrait {}
 
-#[allow(dead_code)]
 struct MyStruct;
 
 impl MyEmptyTraitDummy for MyStruct {}

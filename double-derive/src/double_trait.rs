58be947cd15726313d1c282701c8cd2e04857bd7
Markus Klein <markus.klein@aleph-alpha.com>
2025-06-04 17:26:41 +0200

    feat: Dummy implements all Double traits

---
+++ double-derive/src/double_trait.rs
@@ -0,0 +1,159 @@
+use quote::quote;
+use syn::{
+    FnArg, Ident, ItemTrait, Pat, PatWild, ReturnType, Token, TraitItem, TraitItemFn, Type, parse2,
+    punctuated::Punctuated, spanned::Spanned, token::Comma,
+};
+
+/// Generate a double trait which mirrors the original trait's methods and provides default
+/// implementations using `unimplemented!()`.
+pub fn double_trait(double_trait_name: Ident, org_trait: ItemTrait) -> syn::Result<ItemTrait> {
+    let items = org_trait
+        .items
+        .into_iter()
+        .filter_map(|item| transform_trait_item(item).transpose())
+        .collect::<syn::Result<_>>()?;
+    Ok(ItemTrait {
+        ident: double_trait_name.clone(),
+        items,
+        ..org_trait
+    })
+}
+
+fn transform_trait_item(trait_item: TraitItem) -> syn::Result<Option<TraitItem>> {
+    // We are only interessted in transforming functions
+    let transformed_trait_item = if let TraitItem::Fn(fn_item) = trait_item {
+        transform_function(fn_item)?.map(TraitItem::Fn)
+    } else {
+        // If it is not a function, we forward the original Item
+        Some(trait_item)
+    };
+    Ok(transformed_trait_item)
+}
+
+// Filter method which already have a default implementation
+fn transform_function(mut fn_item: TraitItemFn) -> syn::Result<Option<TraitItemFn>> {
+    if fn_item.default.is_some() {
+        return Ok(None);
+    }
+
+    // We are stripping parameter names in order to avoid warnings regarding unused variables, since
+    // our default implementation is not making use of any arguments.
+    strip_parameter_names(&mut fn_item.sig.inputs);
+
+    let is_impl_future = is_maybe_impl_future(&fn_item.sig.output);
+
+    let default_impl =
+        if is_impl_future {
+            // If the method returns an impl Future, we provide a default implementation using an async
+            // block, so that the compiler won't complain about not being able to infer the type of
+            // `impl Future`.
+            // This `quote!` is falliable, because we do not know for sure that the impl is a future
+            parse2(quote! {{ async { unimplemented!() }} })
+            .map_err(|_| syn::Error::new(
+                fn_item.sig.output.span(),
+                "impl Trait is currently not supported by double-derive. Apart from the special \
+                case of impl Future."))?
+        } else {
+            // Otherwise, we provide a default implementation using unimplemented!
+            // We can unwrap here, this body should always compile
+            parse2(quote! {{ unimplemented!() }}).unwrap()
+        };
+
+    fn_item.default = Some(default_impl);
+
+    Ok(Some(fn_item))
+}
+
+fn strip_parameter_names(input: &mut Punctuated<FnArg, Comma>) {
+    for arg in input {
+        // We are only interested in pattern type. No need to transform `self`
+        if let FnArg::Typed(pat_type) = arg {
+            *pat_type.pat = Pat::Wild(PatWild {
+                attrs: Vec::new(),
+                underscore_token: Token![_](pat_type.span()),
+            })
+        }
+    }
+}
+
+fn is_maybe_impl_future(output: &ReturnType) -> bool {
+    if let ReturnType::Type(_rarrow, ty) = output {
+        if let Type::ImplTrait(ref _impl_trait) = **ty {
+            // Technically, not every impl is a "impl Future", but for now we assume that.
+            true
+        } else {
+            false
+        }
+    } else {
+        false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::double_trait;
+    use quote::quote;
+    use syn::{Ident, ItemTrait, parse2};
+
+    #[test]
+    fn default_impl_for_method_with_impl_future_return() {
+        // Given an original trait with a method returning an impl Future
+        let (double_trait_name, org_trait) = given(
+            quote! { DoubleTrait },
+            quote! {
+                trait OriginalTrait {
+                    fn method(&self) -> impl Future<Output = ()>;
+                }
+            },
+        );
+
+        // When generating the double trait
+        let double_trait = double_trait(double_trait_name, org_trait).unwrap();
+
+        // Then the double trait should have a default implementation for the method which uses
+        // an async block
+        let actual = quote! { #double_trait };
+        let expected = quote! {
+            trait DoubleTrait {
+                fn method(&self) -> impl Future<Output = ()> {
+                    async { unimplemented!() }
+                }
+            }
+        };
+        assert_eq!(actual.to_string(), expected.to_string());
+    }
+
+    #[test]
+    fn strip_parameter_names_from_default_implementation() {
+        // Given an original trait with a method returning an impl Future
+        let (double_trait_name, org_trait) = given(
+            quote! { DoubleTrait },
+            quote! {
+                trait OriginalTrait {
+                    fn method(x: i32);
+                }
+            },
+        );
+
+        // When generating the double trait
+        let double_trait = double_trait(double_trait_name, org_trait).unwrap();
+
+        // Then the double trait should have a default implementation for the method which uses
+        // an async block
+        let actual = quote! { #double_trait };
+        let expected = quote! {
+            trait DoubleTrait {
+                fn method(_: i32) {
+                    unimplemented!()
+                }
+            }
+        };
+        assert_eq!(actual.to_string(), expected.to_string());
+    }
+
+    fn given(attr: proc_macro2::TokenStream, item: proc_macro2::TokenStream) -> (Ident, ItemTrait) {
+        let attr: Ident = parse2(attr).unwrap();
+        let item: ItemTrait = parse2(item).unwrap();
+        (attr, item)
+    }
+}
---
+++ double-derive/src/lib.rs
@@ -0,0 +1,209 @@
+use quote::quote;
+use syn::{Error, Ident, ItemTrait, parse_macro_input};
+
+mod double_trait;
+mod trait_impl;
+
+use self::{double_trait::double_trait, trait_impl::trait_impl};
+
+/// Generates a trait which replicates the original trait method for method. It does implement the
+/// original trait for each of its implementations, by means of forwarding the method calls. The
+/// utility comes from the fact that the generated trait has default implementations for each method
+/// using `unimplemented!()`, which makes it useful for testing purposes.
+///
+/// If a test requires an implementation of an original trait `Org` yet would only invoke one of its
+/// methods, implementing the mirrored method on an implementation of the generated trait `OrgDummy`
+/// is sufficient. The other methods would not be inovked in the test, so their default
+/// implementation using `unimplemented!()` would not be reached.
+///
+/// The argument passed to the attribute is used as the name of the generated trait.
+#[proc_macro_attribute]
+pub fn double(
+    attr: proc_macro::TokenStream,
+    item: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let double_name = parse_macro_input!(attr as Ident);
+    let item = parse_macro_input!(item as ItemTrait);
+
+    let output = expand(double_name, item).unwrap_or_else(Error::into_compile_error);
+
+    proc_macro::TokenStream::from(output)
+}
+
+/// The main implementation of [`crate::double`]. This function is not annotated with
+/// `#[proc_macro_attribute]` so it can exist in unit tests. It uses only APIs build on top of
+/// [`proc_macro2`] in order to be unit testable.
+fn expand(double_trait_name: Ident, org_trait: ItemTrait) -> syn::Result<proc_macro2::TokenStream> {
+    let double_trait = double_trait(double_trait_name.clone(), org_trait.clone())?;
+    let trait_impl = trait_impl(double_trait_name.clone(), org_trait.clone());
+
+    // We generate three items as part of our output.
+    // 1. The orginal trait, which we put in the output unaltered.
+    // 2. The double trait, we genarate, which mirrors the original traits methods and provides
+    //    default implementations using `unimplemented!()`.
+    // 3. An implementation of the original trait for all types which implement the double trait.
+    //    This is done by forwarding the method calls to the double trait.
+    let token_stream = quote! {
+        #org_trait
+
+        #double_trait
+
+        #trait_impl
+
+        impl #double_trait_name for double_trait::Dummy{}
+    };
+    Ok(token_stream)
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::{Ident, expand};
+    use quote::quote;
+    use syn::{ItemTrait, parse2};
+
+    #[test]
+    fn generate_double_trait() {
+        let (attr, item) = given(quote! { MyTraitDummy }, quote! { trait MyTrait {} });
+
+        let output = expand(attr, item).unwrap();
+
+        let expected = quote! {
+            trait MyTrait {}
+
+            trait MyTraitDummy {}
+
+            impl<T> MyTrait for T where T: MyTraitDummy {}
+
+            impl MyTraitDummy for double_trait::Dummy {}
+        };
+        assert_eq!(expected.to_string(), output.to_string());
+    }
+
+    #[test]
+    fn forward_visibility() {
+        // Given a public trait
+        let (attr, item) = given(quote! { MyTraitDummy }, quote! { pub trait MyTrait {} });
+
+        // When generating the dummy
+        let output = expand(attr, item).unwrap();
+
+        // Then the generated trait should be public, too
+        let expected = quote! {
+            pub trait MyTrait {}
+
+            pub trait MyTraitDummy {}
+
+            impl<T> MyTrait for T where T: MyTraitDummy {}
+
+            impl MyTraitDummy for double_trait::Dummy {}
+        };
+        assert_eq!(expected.to_string(), output.to_string());
+    }
+
+    #[test]
+    fn forward_method() {
+        // Given a trait with a method
+        let (attr, item) = given(
+            quote! { MyTraitDummy },
+            quote! {
+                trait MyTrait {
+                    fn foobar(&self);
+                }
+            },
+        );
+
+        // When generating the dummy
+        let output = expand(attr, item).unwrap();
+
+        // Then the generated trait should contain that method, too
+        let expected = quote! {
+            trait MyTrait {
+                fn foobar(&self);
+            }
+
+            trait MyTraitDummy {
+                fn foobar (&self) { unimplemented!() }
+            }
+
+            impl<T> MyTrait for T where T: MyTraitDummy {
+                fn foobar(&self) { <Self as MyTraitDummy>::foobar(self,) }
+            }
+
+            impl MyTraitDummy for double_trait::Dummy {}
+        };
+        assert_eq!(expected.to_string(), output.to_string());
+    }
+
+    #[test]
+    fn respect_existing_default_impl() {
+        // Given a method with a default implementation in the original trait
+        let (attr, item) = given(
+            quote! { MyTraitDummy },
+            quote! {
+                pub trait MyTrait {
+                    fn foobar() { println!("Hello Default!") }
+                }
+            },
+        );
+
+        // When generating the dummy
+        let output = expand(attr, item).unwrap();
+
+        // Then the generated trait should not overide the existing default
+        let expected = quote! {
+            pub trait MyTrait {
+                fn foobar() { println!("Hello Default!") }
+            }
+
+            pub trait MyTraitDummy {}
+
+            impl<T> MyTrait for T where T: MyTraitDummy {
+                fn foobar() { <Self as MyTraitDummy>::foobar() }
+            }
+
+            impl MyTraitDummy for double_trait::Dummy {}
+        };
+        assert_eq!(expected.to_string(), output.to_string());
+    }
+
+    #[test]
+    fn forward_async_method() {
+        // Given a trait with a method
+        let (attr, item) = given(
+            quote! { MyTraitDummy },
+            quote! {
+                trait MyTrait {
+                    async fn foobar(&self);
+                }
+            },
+        );
+
+        // When generating the dummy
+        let output = expand(attr, item).unwrap();
+
+        // Then the generated trait should contain that method, too
+        let expected = quote! {
+            trait MyTrait {
+                async fn foobar(&self);
+            }
+
+            trait MyTraitDummy {
+                async fn foobar (&self) { unimplemented!() }
+            }
+
+            impl<T> MyTrait for T where T: MyTraitDummy {
+                async fn foobar(&self) { <Self as MyTraitDummy>::foobar(self,).await }
+            }
+
+            impl MyTraitDummy for double_trait::Dummy {}
+        };
+        assert_eq!(expected.to_string(), output.to_string());
+    }
+
+    fn given(attr: proc_macro2::TokenStream, item: proc_macro2::TokenStream) -> (Ident, ItemTrait) {
+        let attr: Ident = parse2(attr).unwrap();
+        let item: ItemTrait = parse2(item).unwrap();
+        (attr, item)
+    }
+}
